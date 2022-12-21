ec94f21bb45825a661876ef5ffd8424b2fd8ac74
fguinyard3 <fguinyard3@gmail.com>
2022-12-21 01:17:55 +0000

    first commit

---
+++ .next/static/hXFlwSwVYfspkv349lL1k/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F4a329dfca35bf5c6be5d.css","static\u002Fchunks\u002Fpages\u002Findex-431d87058355e8fbc9c3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4c0e59626af2fe2052a1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
